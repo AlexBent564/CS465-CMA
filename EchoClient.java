7286654678bd42a131930306a82768fa8ab6869f
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2020-01-24 14:03:50 -0700

    Added EchoServer.java and EchoClient.java
    I got these two files from a YouTube tutorial on a multi-threaded echo server. We would need to change some variable and class names to fit the requirements, as well as implement a way so that the server only takes one character at a time from the client, but I figured this gives us something to start off with.

---
+++ EchoClient.java
@@ -0,0 +1,29 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * EchoClient
+ */
+public class EchoClient {
+
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 32000;
+        try (Socket socket = new Socket(host, port)) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner scanner = new Scanner(System.in);
+            String line = null;
+            while (!"exit".equalsIgnoreCase(line)) {
+                line = scanner.nextLine();
+                out.println(line);
+                out.flush();
+                System.out.println("Server replied " + in.readLine());
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
