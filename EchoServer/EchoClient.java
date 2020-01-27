800c5759a41421071aeb5eecf99ae486926bdeff
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-01-27 09:40:05 -0700

    Update EchoClient.java
    added comments

--- EchoServer/EchoClient.java
+++ EchoServer/EchoClient.java
@@ -13,22 +13,27 @@ public class EchoClient {
         int port = 32000;
         // Create socket
         try (Socket socket = new Socket(host, port)) {
+            // prints and reads from the client  
             PrintWriter toClient = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // Reads the inputted data
             Scanner scanner = new Scanner(System.in);
             String line = null;
-            String splitline; //read individual characters in the stream
             // While client is still connected, read and display back what they send
             while (!"quit".equalsIgnoreCase(line)) {
+                // moves to the next line
                 line = scanner.nextLine();
+                // prints that line to the client side
                 toClient.println(line);
                 toClient.flush();
                 String tempoString = fromClient.readLine();
                 // Client disconnects from server
                 if("quit".equalsIgnoreCase(tempoString)){
+                    // if quit exists then disconnect from the server
                     System.out.println("Disconected from server");
                     break;
                 }
+                // print out the server reply
                 System.out.println("Server replied: " + tempoString);
             }
             scanner.close();
