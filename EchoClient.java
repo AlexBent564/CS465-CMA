596aa1388ee6fa39917eb0861531a15a85febbd9
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-01-27 09:44:42 -0700

    Add files via upload

--- EchoClient.java
+++ EchoClient.java
@@ -15,12 +15,18 @@ public class EchoClient {
             BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner scanner = new Scanner(System.in);
             String line = null;
+            String splitline; //read individual characters in the stream
             // While client is still connected, read and display back what they send
             while (!"quit".equalsIgnoreCase(line)) {
                 line = scanner.nextLine();
                 toClient.println(line);
                 toClient.flush();
-                System.out.println("Server replied " + fromClient.readLine());
+                String tempoString = fromClient.readLine();
+                if("quit".equalsIgnoreCase(tempoString)){
+                    System.out.println("Disconected from server");
+                    break;
+                }
+                System.out.println("Server replied: " + tempoString);
             }
             scanner.close();
         } catch (IOException e) {
